852fed8dc76559220d0a28eddaeb7a683a812195
douglasvinicio <douglas.vinicio@hotmail.com>
2020-10-20 04:29:56 -0400

    Orders and order_items / Select pertinent data from orders

--- select.sql
+++ select.sql
@@ -8,29 +8,20 @@ SELECT * FROM order_items;
 SELECT * FROM products;
 SELECT * FROM products_category;
 
-SELECT 
-c.customer_id,
-c.first_name,
-a.country,
-a.city_name,
-a.street_name
+-- Show all billing addresses within customer id, first and last name
+SELECT c.customer_id,c.first_name,a.country,a.city_name,a.street_name
 FROM customers c
-JOIN
-billing_address a On c.customer_id = a.customer_id;
+JOIN billing_address a On c.customer_id = a.customer_id;
 
-
-SELECT 
-c.customer_id,
-c.first_name,
-a.country,
-a.city_name,
-a.street_name
+-- SELECT all customers that has a shipping address attached to the primary key
+SELECT  c.customer_id,c.first_name,a.country,a.city_name,a.street_name
 FROM customers c
-JOIN
-shipping_address a On c.customer_id = a.customer_id;
+JOIN shipping_address a On c.customer_id = a.customer_id;
 
-SELECT a.first_name, b.city_name, c.order_id, d.product_id
+-- SELECT name,city,order_id and product_id inside order_items
+SELECT  c.order_id AS _order, a.first_name,a.last_name, b.country,b.street_name, b.city_name, e.brand,e.model,e.unit_price
 FROM customers a
 JOIN shipping_address b ON a.customer_id = b.customer_id
 JOIN orders c ON c.customer_id = a.customer_id
-JOIN order_items d ON d.order_id = c.order_id;
+JOIN order_items d ON d.order_id = c.order_id
+JOIN products e ON e.product_id = d.product_id;
