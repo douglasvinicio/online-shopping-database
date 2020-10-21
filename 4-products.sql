666e83855c9e52cf9e3ab707907fc922f5aaf8f4
douglasvinicio <douglas.vinicio@hotmail.com>
2020-10-21 18:29:31 -0400

    New File Names

--- 4-products.sql
+++ 4-products.sql
@@ -1,34 +1,45 @@
-#Populating Categories 
+# Creating Categories
 INSERT INTO products_category(category_id,category_name) VALUES (1,"Smartphones");
 INSERT INTO products_category(category_id,category_name) VALUES (2,"Chargers");
 INSERT INTO products_category(category_id,category_name) VALUES (3,"Repair Parts");
 INSERT INTO products_category(category_id,category_name) VALUES (4,"Cases");
 INSERT INTO products_category(category_id,category_name) VALUES (5,"Screen Protector");
 
--- Creating / Populating Products 
 
-#Smartphones - category_id = 1
+# Smartphones
 INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Apple","iPhone 8",329.99,3,1);
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Samsung","Galaxy Fold",899.99,3,1);
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Samsung","Galaxy S9",899.99,3,1);
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Apple","iPhone XR",599.99,3,1);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Samsung","Galaxy Fold",899.99,6,1);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Samsung","Galaxy S9",899.99,7,1);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Apple","iPhone XR",599.99,11,1);
 
-#Chargers - catgeory_id = 2
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Baseus","iPhone Charger 20W",19.99,5,2);
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Belkin","Power Bank 1000mhA - 4 Ports",39.99,10,2);
+# Chargers
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Anker","PowerWave Wireless Charger",11.95,26,2);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("YooTech","Wireless Charger",9.59,42,2);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Anker","PowerCore 10000 Portable Charger",19.99,10,2);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Ainope","USB Car Charger",12.99,4,2);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Samsung","Galaxy Charger",25.95,60,2);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Apple","Officially Licensed Charge Block",34.99,27,2);
 
-#Repair Parts - catgeory_id = 3
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Samsung","Galaxy S10 LCD / Screen Replacement",310.00,3,3);
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Apple","iPhone XR LCD",310.00,10,3);
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Google","Pixel 2 LCD Replacement",210.00,5,3);
+# Repair Parts
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Samsung","Galaxy S9 Replacement Screen",75.95,6,3);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Samsung","Galaxy Fold Replacement Buttons",42.99,9,3);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Samsung","Galaxy S10 Replacement Screen",84.79,10,3);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Google","Google Pixel 2 Replacement Battery",64.95,7,3);
 
-#Cases - catgeory_id = 4
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Otterbox","Shell Case Iphone 7/8 Plus - Yellow",10.00,30,4);
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Otterbox","Defender iPhone 7/8 Plus - Red",10.00,30,4);
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Otterbox","Defender Pro Series iPhone XR - Black",10.00,30,4);
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Tech21","Evo Check Skin iPhone 12",20.00,30,4);
+# Cases
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Spigen","Tough Armor Case",16.95,29,4);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Spigen","Liquid Crystal Case",19.85,21,4);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("JETech","Anti-Scratch Clear Case",6.95,44,4);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("OtterBox","Commuter Series Case",34.99,19,4);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("OtterBox","Defender Series Case",43.61,19,4);
 
-#Screen Protector - catgeory_id = 5
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("InvisibleShield","iPhone 12 PRO MAX",9.99,60,5);
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("InvisibleShield","iPhone 12 PRO",9.99,60,5);
-INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("InvisibleShield","iPhone 12",8.90,60,5);
+# Screen Protectors
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("JETech","Screen Protector (Galaxy S8)",10.95,20,5);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("JETech","Screen Protector (Galaxy S9)",11.95,15,5);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("JETech","Screen Protector (Galaxy Fold)",16.95,4,5);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Mr. Shield","Screen Protector (iPhone 6/6S/7/8)",5.99,48,5);
+INSERT INTO products(brand,model,unit_price,units_in_stock,category_id) VALUES ("Mr. Shield","Screen Protector (iPhone X/XS/XR)",7.99,63,5);
+
+
+
+SELECT * FROM products;
